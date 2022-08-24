8c1ebd26c547bdf9c2a549f2381eeff3ce959951
igpkamrul <kamrulhaider.igp@gmail.com>
2022-08-24 10:23:32 +0600

    ENV

--- index.js
+++ index.js
@@ -6,24 +6,19 @@
  *
  */
 // dependencies
-const { DESTRUCTION } = require('dns');
-const http = require('http');
-const { handleReqRes } = require('./helpers/handleReqRes');
+const http = require("http");
+const { handleReqRes } = require("./helpers/handleReqRes");
+const environment = require("./helpers/environments");
 
 // app object - module scaffolding
 const app = {};
 
-// configuration
-app.config = {
-    port: 3000,
-};
-
 // create server
 app.createServer = () => {
-    const server = http.createServer(app.handleReqRes);
-    server.listen(app.config.port, () => {
-        console.log(`listening to port ${app.config.port}`);
-    });
+  const server = http.createServer(app.handleReqRes);
+  server.listen(environment.port, () => {
+    console.log(`listening to port ${environment.port}`);
+  });
 };
 
 // handle Request Response
